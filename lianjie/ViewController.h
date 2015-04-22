41e2b92eddf7f87c497b70720a79d27aa9c21651
ProgramTracyWang <wangheng88766671@qq.com>
2015-04-22 19:45:41 +0800

    test

--- lianjie/ViewController.h
+++ lianjie/ViewController.h
@@ -10,6 +10,9 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UISegmentedControl *colorChoice;
+@property (weak, nonatomic) IBOutlet UILabel *chosenColor;
+@property (weak, nonatomic) IBOutlet UIWebView *flowerView;
 
 @end
 
--- lianjie/ViewController.m
+++ lianjie/ViewController.m
@@ -13,6 +13,41 @@
 @end
 
 @implementation ViewController
+@synthesize colorChoice;
+@synthesize chosenColor;
+@synthesize flowerView;
+
+-(IBAction)getFlower:(id)sender
+{
+    NSString *outputHTML;
+    NSString *color;
+    NSString *colorVal;
+    int colorNum;
+    colorNum = (int)self.colorChoice.selectedSegmentIndex;
+    switch (colorNum) {
+        case 0:
+            color = @"Red";
+            colorVal = @"red";
+            break;
+        case 1:
+            color = @"Blue";
+            colorVal = @"blue";
+            break;
+        case 2:
+            color = @"Yellow";
+            colorVal = @"yellow";
+            break;
+        case 3:
+            color = @"Green";
+            colorVal = @"green";
+            break;
+    }
+    self.chosenColor.text = [[NSString alloc]initWithFormat:@"%@",color];
+   outputHTML=[[NSString alloc] initWithFormat:@"<body style='margin: 0px; padding: 0px'><img height='300' src='http://www.floraphotographs.com/showrandom.php?color=%@'></body>",colorVal];
+    //outputHTML=[[NSString alloc] initWithFormat:@"<body style='margin: 0px; padding: 0px'><img height='1200' src='http://www.floraphotographs.com/detail.php?imageid=943&startpoint=0&pagetype=3&color=orange&category=%&type='></body>"];
+    [self.flowerView loadHTMLString:outputHTML baseURL:nil];
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
